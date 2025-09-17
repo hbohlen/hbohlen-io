f37d6cd9ea355d32b2d1fa897063e1d5edbd37de
hbohlen <bohlenhayden@gmail.com>
2025-09-16 22:39:28 -0500

    feat(1.1): add initial Nix flake structure

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,19 +1,41 @@
 {
-  description = "A new NixOS configuration";
+  description = "A personal NixOS configuration flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
     home-manager = {
-      url = "github:nix-community/home-manager/0.40.0";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     disko = {
-      url = "github:nix-community/disko/v0.4.0";
+      url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, disko, ... }@inputs: {
-    # NixOS configurations for your machines
+  outputs = { self, nixpkgs, home-manager, disko, ... }: {
+    nixosConfigurations = {
+      # Define your NixOS configurations here
+      # Example:
+      # laptop = nixpkgs.lib.nixosSystem {
+      #   system = "x86_64-linux";
+      #   modules = [
+      #     ./hosts/laptop/configuration.nix
+      #   ];
+      # };
+    };
+
+    homeConfigurations = {
+      # Define your home-manager configurations here
+      # Example:
+      # hbohlen = home-manager.lib.homeManagerConfiguration {
+      #   pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      #   modules = [
+      #     ./users/hbohlen/home.nix
+      #   ];
+      # };
+    };
   };
 }
